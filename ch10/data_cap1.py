328a05e13690cf8f89b71a616ff06b02386f8833
dykim <dykim822@gmail.com>
2021-06-04 12:14:13 +0900

    ch10 upload

---
+++ ch10/data_cap1.py
@@ -0,0 +1,14 @@
+import threading, requests, time
+class HtmlGet(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+    def run(self) -> None:
+        resp = requests.get(self.url)
+        time.sleep(1)
+        print(self.url)
+        print(resp.text)
+print('소스를 보고 싶은 url')
+url = input()
+th = HtmlGet(url)
+th.start()
